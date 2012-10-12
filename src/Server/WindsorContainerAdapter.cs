5b1388af4a6759b8d1066929874d49c6d60908aa
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-10-12 21:03:10 +0200

    Added WindsorAdapter

---
+++ src/Server/WindsorContainerAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Castle.Windsor;
+using ServiceStack.Configuration;
+
+namespace Server
+{
+	public class WindsorContainerAdapter : IContainerAdapter, IRelease, IDisposable
+	{
+		private readonly IWindsorContainer container;
+
+		public WindsorContainerAdapter(IWindsorContainer container)
+		{
+			this.container = container;
+		}
+
+		public T TryResolve<T>()
+		{
+			if (container.Kernel.HasComponent(typeof (T)))
+			{
+				return container.Resolve<T>();
+			}
+
+			return default(T);
+		}
+
+		public T Resolve<T>()
+		{
+			return container.Resolve<T>();
+		}
+
+		public void Dispose()
+		{
+			container.Dispose();
+		}
+
+		public void Release(object instance)
+		{
+			container.Release(instance);
+		}
+	}
+}
