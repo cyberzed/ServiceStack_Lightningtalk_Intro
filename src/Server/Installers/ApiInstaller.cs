070ad716c58ee77c5206bd654a5d874abd22dd66
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-10-12 20:57:34 +0200

    Added bsaic api installer

---
+++ src/Server/Installers/ApiInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using ServiceStack.ServiceHost;
+
+namespace Server.Installers
+{
+	public class ApiInstaller : IWindsorInstaller
+	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			container.Register(
+				Classes.FromThisAssembly()
+					.BasedOn<IService>()
+					.WithService
+					.DefaultInterfaces()
+					.LifestyleSingleton()
+				);
+		}
+	}
+}
