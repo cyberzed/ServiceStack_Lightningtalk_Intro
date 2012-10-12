a385bbca612bc6a0315d9ac17ae43e5d4ed944b4
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-10-12 20:35:46 +0200

    Added AppHost to Server

---
+++ src/Server/AppHost.cs
@@ -0,0 +1,21 @@
+﻿using Funq;
+using ServiceStack.ServiceHost;
+using ServiceStack.WebHost.Endpoints;
+
+namespace Server
+{
+	public class AppHost : AppHostBase
+	{
+		public AppHost() : base("ServiceStack Demo", typeof (AppHost).Assembly)
+		{
+		}
+
+		public override void Configure(Container container)
+		{
+			SetConfig(new EndpointHostConfig
+			          	{
+			          		EnableFeatures = Feature.Json
+			          	});
+		}
+	}
+}
