9d779644c30f3a78af203a64237870be2ba196dc
Stefan D. Poulsen <cyberzed@sleddog.dk>
2012-10-20 17:32:35 +0200

    Added client call

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Client
+﻿using DTO;
+using ServiceStack.ServiceClient.Web;
+
+namespace Client
 {
 	internal class Program
 	{
 		private static void Main()
 		{
+			const string serverPath = "http://localhost/";
+
+			var client = new JsonServiceClient(serverPath);
+
+			var whiskys = client.Get(new Whiskies());
 		}
 	}
 }
